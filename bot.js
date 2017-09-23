9b4a361f1559fc13c80ddea4d1432ac84cb2a384
gco360 [reuben] <31587917+gco360@users.noreply.github.com>
2017-09-23 16:29:25 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,145 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('ready', () => {
+  console.log('gco is ready!');
+  let guilds = bot.guilds.array()
+bot.user.setPresence({game:{name:`use gcohelp | ${guilds.length}`, type:0}})
+  });
+  bot.on("reconnecting", () => {
+      console.log("i was wasting time");
+  });
+
+  bot.on("resume", () => {
+      console.log("im back");
+  });
+bot.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  guild.channels.map(c => c)[0].send(`Please welcome ${member.user} to the server!`);
+});
+  bot.on('guildDelete', guild => {
+    console.log(`I have left ${guild.name} at ${new Date()}`);
+    guild.channels.map(c => c)[0].send(`I have left ${guild.name}`);
+  });
+  bot.on('channelCreate', channel => {
+    console.log(`A ${channel.type} channel by the name of ${channel.name} was created ${channel.createdAt} with the ID of ${channel.id}`);
+    if (channel.type === 'text') return channel.send('You were successful in creating this channel.');
+  });
+bot.on('guildCreate', guild => {
+  console.log(`I have joined  ${guild.name}, owned by ${guild.owner.user.username}`);
+  const channel = guild.channels.find('name', 'gco-log');
+  if (!channel) return;
+channel.send(`i have joined  ${guild.name}, owned by ${guild.owner.user.username} `);
+});
+bot.on('channelDelete', channel => {
+  console.log(`A ${channel.type} by the name of ${channel.name} was successfully deleted.`);
+    guild.channels.map(c => c)[0].send(`channel deleted`);
+});
+bot.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  guild.channels.map(c => c)[0].send(`Please say goodbye to ${member.user.username} we will miss you!`);
+});
+bot.on('guildBanAdd',(guild, user) => {
+    guild.channels.map(c => c)[0].send(`${user.username} was just banned! `);
+});
+bot.on('guildBanRemove',(guild, user) => {
+    guild.channels.map(c => c)[0].send(`${user.username} was just unbanned!`);
+});
+bot.on('guildCreate', guild => {
+  guild.channels.map(c => c)[0].send(`I have joined  ${guild.name}, owned by ${guild.owner.user.username} use \`gco help\`,:pray:  Gco is the first indian bot made by reuben to know more join here https://discord.gg/errvBk2  we to make pokemon games and bot :pray: `);
+});
+  bot.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'gcoavatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+bot.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'gco how are you') {
+    // Send "pong" to the same channel
+    message.channel.send('im fine');
+  }
+});
+bot.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'gco server') {
+    // Send "pong" to the same channel
+    message.channel.send('https://discord.gg/errvBk2 join gco and enjoy');
+  }
+});
+bot.on('message', message => {
+  if (message.content === 'gco owner') {
+    message.channel.send('my owner is master ```reuben``` `https://discord.gg/errvBk2 join here.`');
+   }
+});
+bot.on("message", (message) => {
+  if (message.content.startsWith("gcohelp")) {
+    message.channel.send('oh i just dm you');
+    message.author.send(" ```gco hello - let me say hello then gcopurge -deletes messages and gcorolecreate -creates it own role gcokick -need to have Administrator role, gcoban - need Administrator role```");
+  }
+});
+bot.on('roleCreate', role => {
+  let guild = role.guild;
+  guild.channels.first().send(`A new role has been created :smile: `);
+});
+bot.on('presenceupdate',(oldMember,newMember)  => {
+    let guild = newMember.guild;
+    let playrole = guild.roles.find("name","playing Roblox");
+    let keys = newMember.roles.keyArray();
+
+    if (!playrole) return;
+    if(newMember.user.presence.game && newMember.user.presence.game.name === "Roblox") {
+        newMember.addRole(playRole);
+    } else if (!newMember.user.presence.game && keys.includes(playRole.id)) {
+        newMember.removeRole(playRole);
+    }
+});
+var prefix = 'gco'
+bot.on('message', message => {
+  var guild = message.guild;
+  let args = message.content.split(' ').slice(1);
+  var result = args.join(' ');
+
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(prefix + 'rolecreate')) {
+    guild.createRole({name:'A Gco\'s role', color:'#00FFFF', mentionable:true}).catch(error => console.log(error));
+  } else
+
+  if (message.content.startsWith(prefix + 'purge')) {
+    let messagecount = parseInt(result);
+    message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+  } else
+
+  if (message.content.startsWith(prefix + 'ping')) {
+    message.channel.send(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
+  } else
+  if (message.content.startsWith(prefix + 'hello')) {
+    message.channel.send(':slight_smile:  hey man dont disturb use gcohelp');
+  } else
+  if (message.content.startsWith(prefix + 'kick')) {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator","Mod","Admin"].includes(r.name)) )
+     return message.reply("Sorry, you don't have permissions to use this!");
+   let member = message.mentions.members.first();
+   if(!member)
+     return message.reply("Please mention a valid member of this server");
+   if(!member.kickable)
+     return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+  } else
+  if (message.content.startsWith(prefix + 'ban')) {
+  if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+     return message.reply("Sorry, you don't have permissions to use this!");
+
+   let member = message.mentions.members.first();
+   if(!member)
+     return message.reply("Please mention a valid member of this server");
+   if(!member.bannable)
+return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+  } 
+});
+bot.login('process.env.BOT_TOKEN');
